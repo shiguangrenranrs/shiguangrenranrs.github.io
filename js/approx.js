953e217eb4ae8299ea43506343bdcc79b58c48c1
shiguangrenranrs <3142996898@qq.com>
2022-02-26 20:37:01 +0800

    开学前的提交，增加了扩大倍数功能，更新了颜色风格；

--- js/approx.js
+++ js/approx.js
@@ -1,1 +1,50 @@
-function compute_fun(e,l){let t=1,o=Math.min(e,l);for(let a=2;a<=o;a++)e%a==0&&l%a==0&&(t=a);return e/=t,l/=t,e+"/"+l}let molecular=$(".approx #Molecular"),denominator=$(".approx #Denominator"),compute=$(".approx button"),result=$(".approx #Result");compute.click(()=>{let e=parseInt(molecular.val()),l=parseInt(denominator.val());console.log(e,l),isNaN(e)||isNaN(l)?alert("输入的数据格式不正确"):e<=0||l<=0?alert("要计算的值不能小于或等于0"):e>=1e7||l>=1e7?alert("要计算的值过大"):result.val(compute_fun(e,l))}),$("#svg-link").click(()=>!1);
+let fenzi1 = $('.approx #beforeNum1');
+let fenmu1 = $('.approx #beforeNum2');
+let fenzi2 = $('.approx #afterNum1');
+let fenmu2 = $('.approx #afterNum2');
+let mul = $('.approx .control-box input');
+let div = $('.approx .control-box button');
+
+mul.bind('input', () => {
+    let fenzi_value = parseInt(fenzi2.val());
+    let fenmu_value = parseInt(fenmu2.val());
+    let m = parseInt(mul.val());
+    console.log(fenzi_value, fenmu_value);
+
+    if (isNaN(fenzi_value) || isNaN(fenmu_value)) {
+        alert('输入的数据格式不正确');
+        return;
+    }
+    if (isNaN(m)) {
+        return;
+    }
+    fenzi1.val(fenzi_value * m);
+    fenmu1.val(fenmu_value * m);
+})
+div.click(() => {
+    let fenzi_value = parseInt(fenzi1.val());
+    let fenmu_value = parseInt(fenmu1.val());
+    console.log(fenzi_value, fenmu_value);
+
+    if (isNaN(fenzi_value) || isNaN(fenmu_value)) {
+        alert('输入的数据格式不正确');
+        return;
+    }
+    let z = compute_fun(fenzi_value, fenmu_value);
+    fenzi2.val(fenzi_value / z);
+    fenmu2.val(fenmu_value / z);
+});
+
+function compute_fun(attr1, attr2) {
+    let z = 1;
+    let min = Math.min(attr1, attr2);
+    for (let i = 2; i <= min; i++) {
+        if (attr1 % i == 0 && attr2 % i == 0) {
+            z = i;
+        }
+    }
+    return z;
+}
+$('#svg-link').click(() => {
+    return false;
+});
