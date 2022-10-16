61ead4be6ae516ae56ff764932cc73f65d125b0b
shiguangrenranrs <3142996898@qq.com>
2022-10-16 22:46:34 +0800

    展示项目

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,31 @@
 <template>
   <main>
-    <nav-bar />
-    <div class="container">
-      <puzzle />
-      <computed />
-      <modulo-2-division />
-    </div>
-    <bottom-bar :updateDate="updateDate"/>
+    <p>Main Page</p>
   </main>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
-import Puzzle from "./components/Puzzle.vue";
-import Computed from "./components/Computed.vue";
-import BottomBar from "./components/BottomBar.vue";
-import Modulo2Division from "./components/Modulo2Division.vue";
 
 export default {
   name: "App",
-  data(){
-    return {
-      updateDate: "2022-06-19"
-    }
-  },
-  components: {
-    NavBar,
-    Puzzle,
-    Computed,
-    BottomBar,
-    Modulo2Division,
-  },
 };
 </script>
 
-<style>
+<style scoped>
 * {
   margin: 0;
   padding: 0;
 }
-a {
-  text-decoration: none;
-}
+
 body {
   background-color: #f4f4f4;
-  /* overflow: hidden; */
+  font-family: consolas;
 }
-
-.container {
-  line-height: normal;
-  margin: 0 auto;
-  min-height: 600px;
+main{
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  align-content: space-around;
-}
-.container > section {
-  margin-top: 20px;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  color: #888;
 }
 </style>
